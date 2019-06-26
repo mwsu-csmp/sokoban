6e64c80d153d8cc5eed42c6afb90eae8b43d43f5
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-06-26 14:16:49 -0500

    added spring boot basics

---
+++ src/main/java/edu/missouriwestern/csmp/gg/sokoban/SokobanApp.java
@@ -0,0 +1,12 @@
+package edu.missouriwestern.csmp.gg.sokoban;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokobanApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SokobanApp.class, args);
+    }
+}
