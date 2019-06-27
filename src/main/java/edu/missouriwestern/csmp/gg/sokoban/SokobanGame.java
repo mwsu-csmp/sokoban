4cc5fd5527251fe2810407a560d66987b46a6a0a
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-06-26 22:57:26 -0500

    automatically discover room beans for game

--- src/main/java/edu/missouriwestern/csmp/gg/sokoban/SokobanGame.java
+++ src/main/java/edu/missouriwestern/csmp/gg/sokoban/SokobanGame.java
@@ -1,6 +1,23 @@
 package edu.missouriwestern.csmp.gg.sokoban;
 
+import edu.missouriwestern.csmp.gg.base.Board;
 import edu.missouriwestern.csmp.gg.base.Game;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.logging.Logger;
 
 public class SokobanGame extends Game {
+
+    private static Logger logger = Logger.getLogger(SokobanGame.class.getCanonicalName());
+
+    /** loads boards at start of server */
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        var maps = event.getApplicationContext().getBeansOfType(Board.class);
+        for(var mapName : maps.keySet()) {
+            this.addBoard(mapName, maps.get(mapName));
+            logger.info(mapName + maps.get(mapName));
+        }
+    }
 }
