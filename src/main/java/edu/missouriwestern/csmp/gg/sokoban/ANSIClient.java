b5f71c65c085bfa2bd2f578bd85bfc7f7ddcf5ce
Joseph Kendall-Morwick <jbmorwick@gmail.com>
2019-07-07 19:09:20 -0500

    added basic start for ansi terminal server (#1)

--- src/main/java/edu/missouriwestern/csmp/gg/sokoban/ANSIClient.java
+++ src/main/java/edu/missouriwestern/csmp/gg/sokoban/ANSIClient.java
@@ -1,4 +1,46 @@
 package edu.missouriwestern.csmp.gg.sokoban;
 
-public class ANSIClient {
+import edu.missouriwestern.csmp.gg.base.Game;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.logging.Logger;
+
+public class ANSIClient implements Runnable {
+
+    private static Logger logger = Logger.getLogger(SokobanGame.class.getCanonicalName());
+
+    private final Socket socket;
+    private final ANSIGameServer server;
+    private final PrintWriter out;
+    private final Game game;
+
+    public ANSIClient(ANSIGameServer server, Socket socket) throws IOException {
+        this.socket = socket;
+        this.server = server;
+        this.game = server.getGame();
+        this.out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+    }
+
+    @Override
+    public void run() {
+        logger.info("starting client handler");
+        // TODO: login
+
+        // TODO: command loop
+
+        // temporary test code
+        out.println(game.getBoard("foyer"));out.flush();
+
+        // TODO: cleanup / remove from server
+        logger.info("client complete, cleaning up");
+        try {
+            socket.close();
+        } catch(IOException e) {
+            logger.info("failed to close client connection: " + e);
+        }
+        server.removeClient(this);
+    }
 }
--- src/main/java/edu/missouriwestern/csmp/gg/sokoban/ANSIGameServer.java
+++ src/main/java/edu/missouriwestern/csmp/gg/sokoban/ANSIGameServer.java
@@ -1,4 +1,55 @@
 package edu.missouriwestern.csmp.gg.sokoban;
 
+import edu.missouriwestern.csmp.gg.base.Game;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.List;
+import java.util.Vector;
+import java.util.logging.Logger;
+
+@Component("server")
 public class ANSIGameServer {
+
+    private static Logger logger = Logger.getLogger(SokobanGame.class.getCanonicalName());
+
+    public final int PORT = 9000;
+    private final List<ANSIClient> clients = new Vector<ANSIClient>();
+    private final TaskExecutor taskExecutor;
+
+    private final ServerSocket socket;
+    private final Game game;
+
+    public ANSIGameServer(@Autowired Game game, @Autowired TaskExecutor taskExecutor) throws IOException {
+        socket = new ServerSocket(PORT);
+        this.game = game;
+        this.taskExecutor = taskExecutor;
+        var server = this; // needed for anonymous class below
+        // launch socket listening thread
+        taskExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                logger.info("Starting up ANSI terminal server");
+                while(true) {
+                    try {
+                        var client = new ANSIClient(server, socket.accept());
+                        logger.info("accepting incoming client connection");
+                        taskExecutor.execute(client);
+                        clients.add(client);
+                    } catch (IOException e) {
+                        logger.info("Error connecting client: " + e);
+                    }
+                }
+            }
+        });
+    }
+
+    public Game getGame() { return game; }
+
+    public void removeClient(ANSIClient client) {
+        clients.remove(client);
+    }
 }
